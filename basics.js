ad483cc197b24c82e78188b378eabd0acda07a4a
Scott Joseph-Smith <hubblejs@gmail.com>
2018-03-05 19:47:19 -1000

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,24 +1,51 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = 'Scott'
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
 
+var person = {
+    [name: 'Scott'],
+    [age: 37]
+}
+
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+
+var canDrive = false;
+function license (){
+    if person.age >= 16 {
+        canDrive === true;
+    } else {
+        canDrive === false;
+    }
+}
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
-
+function greet (){
+    document.write('Hello, my name is ' + person.name);
+}
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
-
+var dataTypes = ['one', 1, true, null, undefined, Symbol()]
 /* Create a `dog` object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+
+ var dog = {
+     [name: 'Spot'];
+     function bark () {
+         if (dog.name === 'Spot') {
+             alert('bark')
+         }
+     }
+ }
